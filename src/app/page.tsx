e0d866e1f659ad3691f58a40e69ab04d8bc79d31
ThamiBa <baladithami@gmail.com>
2024-06-04 11:23:17 +0100

    Buy_Me_Coffee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <div>test</div>
+    <section className="max-w-lg mx-auto text-center">
+      <h1 className="text-6xl font-bold">
+        Fund your <br/>
+        craetive work
+      </h1>
+      <h2 className="mt-4 mb-8">
+        Accept support for your work.<br/>
+        Its easier than you think.
+      </h2>
+      <button className="bg-yellow-300 px-8 py-4 font-bold rounded-full">
+        Start my page
+      </button>
+    </section>
   );
 }
